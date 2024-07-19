1a781df483555260c991a690fd732ff4390039a6
Blade0 <m.grearson171110@outlook.com>
2024-07-19 17:13:09 +0100

    man fuck

---
+++ src/install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+flatpak install flathub com.github.GradienceTeam.Gradience com.raggesilver.BlackBox
+cd .config
+mv * ~/.config
+cd ..
+cd extensions
+mv * ~/.local/share/gnome-shell/extensions
+cd ..
+mv saved_settings.dconf ~
+cd ~
+dconf load -f / < saved_settings.dconf
+cd $OLDPWD
+mv blackbox-theme.json ~/.var/app/com.raggesilver.BlackBox/data/blackbox/schemes
+echo 'export PATH=$PATH:/home/blade0/.local/bin' >> ~/.bashrc
+curl -s https://ohmyposh.dev/install.sh | bash -s
+echo 'eval "$(oh-my-posh init bash --config ~/.config/ohmyposh.omp.json)"' >> ~/.bashrc
+echo '---------------------------------------------------------'
+echo 'Refer to the github to continue'
